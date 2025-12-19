559fe11d12e8100eea821a1436a7838d511b82a1
Tharaka Thusith <www.tharaka01.2000@gmail.com>
2025-12-19 23:19:36 +0530

    Refactor script.js to toggle navbar menu
    Removed getperimiter function and added menu toggle functionality.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function getperimiter(radius){
-    let perimiter = 2 * 3.14*radius;
-    return perimiter;
-}
-let p = getperimiter(5);
-console.log(p);
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle("active");
+};
